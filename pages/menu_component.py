4826a944795169230148a89ac39696cb80c38f32
Ricardo Espindola <rae.apollo@hotmail.com>
2026-06-10 15:05:07 -0300

    Cambios varios

--- pages/menu_component.py
+++ pages/menu_component.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pyautogui
+import time
 
 class MenuComponent:
     def __init__(self, driver):
@@ -9,9 +11,87 @@ class MenuComponent:
 
         self.btn_nueva_reserva = (By.XPATH, "//span[contains(text(), 'Nueva reserva de remis')]") # Botón para abrir el menú de nueva reserva
         self.btn_nuevo_envio = (By.XPATH, "//span[contains(text(), 'Nuevo envío')]") # Botón para abrir el menú de nuevo envío
+        self.btn_nueva_mensajeria = (By.XPATH, "//span[contains(text(), 'Nueva mensajería')]") # Botón para abrir el menú de nueva mensajería (si existe)
+        self.btn_reserva_de_asiento_charter = (By.XPATH, "//span[contains(text(), 'Reserva de asiento (Chárter)')]") # Botón para abrir el menú de reserva de asiento charter (si existe)    
+        self.btn_logistica_corporativa = (By.XPATH, "//span[contains(text(), 'Logística corporativa')]") # Botón para abrir el menú de logística corporativa (si existe)
+        self.btn_seguimiento_de_viajes = (By.XPATH, "//span[contains(text(), 'Seguimiento de viajes')]") # Botón para abrir el menú de seguimiento de viajes (si existe)
+        self.btn_mis_reservas_de_remis = (By.XPATH, "//span[contains(text(), 'Mis reservas de remis')]") # Botón para abrir el menú de mis reservas de remis (si existe)
+        self.btn_mis_envios_mensajerias = (By.XPATH, "//span[contains(text(), ' Mis envíos / Mensajería')]") # Botón para abrir el menú de mis envíos y mensajería (si existe)
+        self.btn_estado_de_envios = (By.XPATH, "//span[contains(text(), 'Estado de envíos')]") # Botón para abrir el menú de estado de envíos (si existe)
+        self.btn_mis_reservas_de_charter = (By.XPATH, "//span[contains(text(), 'Mis reservas de chárter')]") # Botón para abrir el menú de mis reservas de chárter (si existe)
+        self.btn_gestion_de_servicios_charter = (By.XPATH, "//span[contains(text(), 'Gestión de servicios chárter')]") # Botón para abrir el menú de gestión de servicios chárter (si existe)
+        self.btn_mi_perfil = (By.XPATH, "//span[contains(text(), 'Mi perfil')]") # Botón para abrir el menú de mi perfil (si existe)
+        self.btn_gestion_de_pasajeros = (By.XPATH, "//span[contains(text(), 'Gestión de pasajeros')]") # Botón para abrir el menú de gestión de pasajeros (si existe)
+        self.btn_medios_de_pago = (By.XPATH, "//span[contains(text(), 'Medios de pago')]") # Botón para abrir el menú de medios de pago (si existe)
+        self.btn_soporte_telefonico = (By.XPATH, "//span[contains(text(), 'Soporte (telefónico)')]") # Botón para abrir el menú de soporte telefónico (si existe)
+        self.btn_puntos_de_encuentro = (By.XPATH, "//span[contains(text(), 'Puntos de encuentro')]") # Botón para abrir el menú de puntos de encuentro (si existe)  
+        self.btn_cambiar_contraseña = (By.XPATH, "//span[contains(text(), 'Cambiar contraseña')]") # Botón para abrir el menú de cambiar contraseña (si existe)
+        self.btn_terminos_y_condiciones = (By.XPATH, "//span[contains(text(), 'Términos y condiciones')]") # Botón para abrir el menú de términos y condiciones (si existe) 
+        self.btn_cerrar_sesion = (By.XPATH, "//span[contains(text(), 'Cerrar sesión')]") # Botón para cerrar sesión (si existe)
+        self.btn_cerrar_modal_puntos = (By.XPATH, "//button[contains(@class, 'at-icon-button')]//mat-icon[text()='close']") # Botón para cerrar el modal de puntos de encuentro (si aparece)
+        self.btn_aceptar_condiciones = (By.XPATH, "//button[contains(., 'ACEPTAR')]") # Botón para aceptar términos y condiciones (si aparece)
     def ir_a_nueva_reserva_de_remis(self):
         elemento = self.wait.until(EC.element_to_be_clickable(self.btn_nueva_reserva)) # Esperamos a que el botón de nueva reserva sea clickeable
         elemento.click() # Hacemos click en el botón de nueva reserva
     def ir_a_nuevo_envio(self):
         elemento = self.wait.until(EC.element_to_be_clickable(self.btn_nuevo_envio)) # Esperamos a que el botón de nuevo envío sea clickeable
         elemento.click() # Hacemos click en el botón de nuevo envío 
+    def ir_a_nueva_mensajeria(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_nueva_mensajeria)) # Esperamos a que el botón de nueva mensajería sea clickeable
+        elemento.click() # Hacemos click en el botón de nueva mensajería
+        btn_aceptar = self.wait.until(EC.presence_of_element_located(self.btn_aceptar_condiciones)) # Esperamos a que el botón de aceptar términos y condiciones sea clickeable
+        self.driver.execute_script("arguments[0].click();", btn_aceptar) # Hacemos click en el botón de aceptar términos y condiciones utilizando JavaScript (esto puede ayudar a evitar problemas de elementos superpuestos o no clickeables)
+        time.sleep(2) # Esperamos 2 segundos para que se procese el click en aceptar términos y condiciones (puedes ajustar este tiempo según sea necesario)
+    def ir_a_reserva_de_asiento_charter(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_reserva_de_asiento_charter)) # Esperamos a que el botón de reserva de asiento charter sea clickeable
+        elemento.click() # Hacemos click en el botón de reserva de asiento charter
+    def ir_a_logistica_corporativa(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_logistica_corporativa)) # Esperamos a que el botón de logística corporativa sea clickeable
+        elemento.click() # Hacemos click en el botón de logística corporativa
+    def ir_a_seguimiento_de_viajes(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_seguimiento_de_viajes)) # Esperamos a que el botón de seguimiento de viajes sea clickeable
+        elemento.click() # Hacemos click en el botón de seguimiento de viajes
+    def ir_a_mis_reservas_de_remis(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_mis_reservas_de_remis)) # Esperamos a que el botón de mis reservas de remis sea clickeable
+        elemento.click() # Hacemos click en el botón de mis reservas de remis
+    def ir_a_mis_envios_mensajerias(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_mis_envios_mensajerias)) # Esperamos a que el botón de mis envíos y mensajería sea clickeable
+        elemento.click() # Hacemos click en el botón de mis envíos y mensajería
+    def ir_a_estado_de_envios(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_estado_de_envios)) # Esperamos a que el botón de estado de envíos sea clickeable
+        elemento.click() # Hacemos click en el botón de estado de envíos
+    def ir_a_mis_reservas_de_charter(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_mis_reservas_de_charter)) # Esperamos a que el botón de mis reservas de chárter sea clickeable
+        elemento.click() # Hacemos click en el botón de mis reservas de chárter
+    def ir_a_gestion_de_servicios_charter(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_gestion_de_servicios_charter)) # Esperamos a que el botón de gestión de servicios chárter sea clickeable
+        elemento.click() # Hacemos click en el botón de gestión de servicios chárter
+    def ir_a_mi_perfil(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_mi_perfil)) # Esperamos a que el botón de mi perfil sea clickeable
+        elemento.click() # Hacemos click en el botón de mi perfil
+    def ir_a_gestion_de_pasajeros(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_gestion_de_pasajeros)) # Esperamos a que el botón de gestión de pasajeros sea clickeable
+        elemento.click() # Hacemos click en el botón de gestión de pasajeros
+    def ir_a_medios_de_pago(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_medios_de_pago)) # Esperamos a que el botón de medios de pago sea clickeable
+        elemento.click() # Hacemos click en el botón de medios de pago
+    def ir_a_soporte_telefonico(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_soporte_telefonico)) # Esperamos a que el botón de soporte telefónico sea clickeable
+        elemento.click() # Hacemos click en el botón de soporte telefónico
+        time.sleep(2) # Esperamos 2 segundos para que se abra el modal de soporte telefónico (puedes ajustar este tiempo según sea necesario)
+        pyautogui.press('esc') # Presionamos la tecla 'Esc' para la ventana emergente de soporte telefónico (puedes ajustar esta lógica según sea necesario)
+        time.sleep(1) # Esperamos 1 segundo para asegurarnos de que el modal se haya cerrado (puedes ajustar este tiempo según sea necesario)
+    def ir_a_puntos_de_encuentro(self):
+        self.wait.until(EC.element_to_be_clickable(self.btn_puntos_de_encuentro)) # Esperamos a que el botón de puntos de encuentro sea clickeable
+        elemento_x = self.wait.until(EC.element_to_be_clickable(self.btn_puntos_de_encuentro)) # Esperamos a que el botón de puntos de encuentro sea clickeable
+        elemento_x.click() # Hacemos click en el botón de puntos de encuentro
+    def ir_a_cambiar_contraseña(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_cambiar_contraseña)) # Esperamos a que el botón de cambiar contraseña sea clickeable
+        elemento.click() # Hacemos click en el botón de cambiar contraseña
+    def ir_a_terminos_y_condiciones(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_terminos_y_condiciones)) # Esperamos a que el botón de términos y condiciones sea clickeable
+        elemento.click() # Hacemos click en el botón de términos y condiciones
+    def cerrar_sesion(self):
+        elemento = self.wait.until(EC.element_to_be_clickable(self.btn_cerrar_sesion)) # Esperamos a que el botón de cerrar sesión sea clickeable
+        elemento.click() # Hacemos click en el botón de cerrar sesión para cerrar la sesión actual (puedes ajustar esta lógica según sea necesario) 
+    
--- tests/test_chrome.py
+++ tests/test_chrome.py
@@ -1,14 +1,18 @@
 import pytest
 import time
+import pyautogui
+from faker import Faker 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options # Importamos las librerías necesarias para Selenium y WebDriver Manager
-from pages.login_page import LoginPage # Importamos la clase LoginPage desde el archivo login_page.py
-from pages.menu_component import MenuComponent # Importamos la clase MenuComponent desde el archivo menu_component.py
+from selenium.webdriver.chrome.options import Options 
+from pages.login_page import LoginPage 
+from pages.menu_component import MenuComponent 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pages.reserva_page import ReservaPage # Importamos la clase ReservaPage desde el archivo reserva_page.py   
+from pages.reserva_page import ReservaPage    
+
+fake = Faker()
 
 @pytest.fixture
 def driver():
@@ -24,11 +28,82 @@ def driver():
     chrome_options.add_experimental_option("useAutomationExtension", False)
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--disable-popup-blocking")
-    # Configuramos el driver de Chrome utilizando webdriver_manager
+# Configuramos el driver de Chrome utilizando webdriver_manager
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
     yield driver
     driver.quit() # Cerramos el navegador al finalizar la prueba
+# CASO 1 : Login fallido con credenciales aleatorias generadas por Faker
+def test_login_fallido_con_faker(driver):
+    login_page = LoginPage(driver) # Creamos una instancia de LoginPage
+    user_random = fake.email() # Generamos un nombre de usuario aleatorio con Faker
+    password_random = fake.password() # Generamos una contraseña aleatoria con Faker
+    driver.get("https://clientes.traslada.com.ar/auth/login") # Navegamos a la página de login
+    login_page.ingresar_credenciales(user_random, password_random) # Ingresamos las credenciales aleatorias
+    mensaje_esperado = "Las credenciales ingresadas son incorrectas. Por favor, intente nuevamente." # Mensaje de error esperado
+    mensaje_actual = login_page.obtener_mensaje_error() # Obtenemos el mensaje de error actual
+    assert mensaje_esperado in mensaje_actual, f"Esperaba '{mensaje_esperado}' pero llegó '{mensaje_actual}'"
+    time.sleep(2) # Esperamos 2 segundos para observar el resultado (puedes ajustar este tiempo según sea necesario)
+    login_page.cerrar_mensaje_error() # Cerramos el mensaje de error para limpiar la interfaz
+    time.sleep(2) # Esperamos 2 segundos para observar el resultado (puedes ajustar este tiempo según sea necesario)
+
+# CASO 2 : Login exitoso con credenciales válidas y navegación a la sección de nueva reserva de remis
+def test_login_exitoso_y_navegacion_a_nueva_reserva_de_remis(driver):
+    login_page = LoginPage(driver) # Creamos una instancia de LoginPage
+    menu = MenuComponent(driver) # Creamos una instancia de MenuComponent
+    driver.get("https://clientes.traslada.com.ar/auth/login") # Navegamos a la página de login
+    login_page.ingresar_credenciales("ehs@sommytech.com.ar", "123456") # Ingresamos las credenciales de prueba
+    time.sleep(2) # Esperamos 2 segundos para que se procese el login (puedes ajustar este tiempo según sea necesario)
+    menu.ir_a_nueva_reserva_de_remis() # Navegamos al menú de nueva reserva de remis
+    WebDriverWait(driver, 10).until(EC.url_contains("booking")) # Esperamos a que la URL contenga "booking"
+    except_url_part = "booking"
+    assert except_url_part in driver.current_url, f"Se esperaba que la URL contenga '{except_url_part}' pero se obtuvo '{driver.current_url}'" # Verificamos que la URL contenga la parte esperada
+    time.sleep(5) # Esperamos 5 segundos para observar el resultado (puedes ajustar este tiempo según sea necesario)    
+ 
+ # CASO 3 : Recorrido completo del menu de cliente después de un login exitoso
+def test_recorrido_completo_del_menu_despues_de_login_exitoso(driver):
+    login_page = LoginPage(driver) # Creamos una instancia de LoginPage
+    menu = MenuComponent(driver) # Creamos una instancia de MenuComponent
+    driver.get("https://clientes.traslada.com.ar/auth/login") # Navegamos a la página de login
+    login_page.ingresar_credenciales("ehs@sommytech.com.ar", "123456") # Ingresamos las credenciales de prueba
+    WebDriverWait(driver, 10).until(lambda d: "services" in d.current_url or "booking" in d.current_url) # Esperamos a que la URL contenga "services" o "booking"
+    menu.ir_a_nueva_reserva_de_remis() # Navegamos al menú de nueva reserva
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación 
+    menu.ir_a_nuevo_envio() # Navegamos al menú de nuevo envío
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación 
+    menu.ir_a_nueva_mensajeria() # Navegamos al menú de nueva mensajería
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación 
+    menu.ir_a_reserva_de_asiento_charter() # Navegamos al menú de reserva de asiento charter
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_logistica_corporativa() # Navegamos al menú de logística corporativa
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_seguimiento_de_viajes() # Navegamos al menú de seguimiento de viajes
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_mis_reservas_de_remis() # Navegamos al menú de mis reservas de remis
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_mis_envios_mensajerias() # Navegamos al menú de mis envíos y mensajería
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_estado_de_envios() # Navegamos al menú de estado de envíos
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_mis_reservas_de_charter() # Navegamos al menú de mis reservas de chárter
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_gestion_de_servicios_charter() # Navegamos al menú de gestión de servicios chárter
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_mi_perfil() # Navegamos al menú de mi perfil
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_gestion_de_pasajeros() # Navegamos al menú de gestión de pasajeros
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_medios_de_pago() # Navegamos al menú de medios de pago
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_soporte_telefonico() # Navegamos al menú de soporte telefónico
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_puntos_de_encuentro() # Navegamos al menú de puntos de encuentro
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_cambiar_contraseña() # Navegamos al menú de cambiar contraseña
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.ir_a_terminos_y_condiciones() # Navegamos al menú de términos y condiciones
+    time.sleep(2) # Esperamos 2 segundos para que se procese la navegación
+    menu.cerrar_sesion() # Cerramos sesión al finalizar el recorrido del menú   
 
 def test_login_clientes_traslada_web(driver):
     login_page = LoginPage(driver) # Creamos una instancia de LoginPage
@@ -49,4 +124,4 @@ def test_login_clientes_traslada_web(driver):
     WebDriverWait(driver, 10).until(EC.url_contains("booking")) # Esperamos a que la URL contenga "booking"
     except_url_part = "booking"
     assert except_url_part in driver.current_url, f"Se esperaba que la URL contenga '{except_url_part}' pero se obtuvo '{driver.current_url}'" # Verificamos que la URL contenga la parte esperada
-    time.sleep(5) # Esperamos 5 segundos para observar el resultado (puedes ajustar este tiempo según sea necesario)
+    time.sleep(5) # Esperamos 5 segundos para observar el resultado (puedes ajustar este tiempo según sea necesar

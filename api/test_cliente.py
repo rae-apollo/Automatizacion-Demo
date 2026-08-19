c890c7af23c08d4faec5941b067f5cd553f0ed17
Ricardo Espindola <rae.apollo@hotmail.com>
2026-08-19 10:37:19 -0300

    Se agregan archivos al git ignore y se agregan markers para pruebas de api

--- api/test_cliente.py
+++ api/test_cliente.py
@@ -1,15 +1,17 @@
 import requests
+import pytest
 
-def test_obtner_pasajeros(api_url, token_cliente):
+# Marcador para indicar que esta prueba es de tipo GET.
+@pytest.mark.get 
+# Función de prueba para obtener la lista de pasajeros.
+def test_obtener_pasajeros(api_url, token_cliente):
     endpoint = f"{api_url}/passengers/previews"
-
     params = {
         "pageIndex": 1,
         "pageSize": 50,
         "orderBy": "userName",
         "sortOrder": "Ascending"
     }
-
     headers = {
         "Access-Token": token_cliente
     }
